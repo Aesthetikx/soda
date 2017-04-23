eac9c1b6c6ca136d9389431dee47d28306fb857b
John DeSilva <desilvjo@umich.edu>
2017-04-23 15:33:28 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def ingredient
+  food = Faker::Food
+  "#{food.measurement} #{food.send([:ingredient, :spice].sample)}"
+end
+
+def make_instructions
+  Array.new([*4..10].sample) { ingredient }.join(', ')
+end
+
+def make_recipe(title)
+  Recipe.create!(title: title, instructions: make_instructions)
+end
+
+Company.create!(name: 'Coke')
+['Coca-Cola', 'Sprite', 'Mellow Yellow'].each(&method(:make_recipe))
+
+Company.create!(name: 'Pepsi')
+['Pepsi', 'Mountain Dew', 'Mug Root Beer'].each(&method(:make_recipe))
