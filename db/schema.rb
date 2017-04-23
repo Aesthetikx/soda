aa149b54d84693d262f6feced84a94e25e57a3fd
John DeSilva <desilvjo@umich.edu>
2017-04-23 19:37:11 -0400

    Create Companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170423175927) do
+ActiveRecord::Schema.define(version: 20170423233622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
